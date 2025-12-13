964f9984f12efe4b01454d8d4ee0b46ab5d106ff
Alexey <168357108+ago106@users.noreply.github.com>
2025-12-14 00:15:43 +0700

    Auto update LuaCore время также 2025-12-14 00:15:42

---
+++ LuaCore/LuaCore.lua
@@ -0,0 +1,7 @@
+-- Auto LuaCore время также 2025-12-14 00:15:42
+local LuaCore = {
+   Protect = "i0e81f1h3tap182md8ibf3tyl2j16pcx",
+   Api = "2cb6cf25e8d1facef1cf14c067ccfc41",
+   Id = "8dcc7ed0-108e-41a8-8f90-71b9ac9d5d93",
+};
+return LuaCore
