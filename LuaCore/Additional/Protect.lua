7596fcd55b3c5a6d58eac461b352d6357b1ae7e3
Alexey <168357108+ago106@users.noreply.github.com>
2025-12-15 00:27:46 +0700

    LuaCore Protection 2025-12-15 00:27:45

--- LuaCore/Additional/Protect.lua
+++ LuaCore/Additional/Protect.lua
@@ -1,7 +1,7 @@
--- Protection 2025-12-15 00:22:54
+-- Protection 2025-12-15 00:27:45
 local LuaCore = {
-   Protect = "q13kaisfv14xxdteob4cqb6q252ymi6u",
-   Api = "742098d21f1d81bafafa2c36adf635e3",
-   Id = "7027c043-1a29-454e-91df-ffc5efcf7794",
+   Protect = "njmv784wnq6jm9k8k8zy0y2tvi01nqml",
+   Api = "378afeda6eed47fd03c6c42fdea1bdc9",
+   Id = "83e4cde4-9627-4caf-a0e8-43d2389005fb",
 };
 return LuaCore
